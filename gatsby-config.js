a99dc16a8771437c6a4fbc1550f52a251730d9f1
tony gonzalez <agonzalez@tonys-MacBook-Pro.local>
2023-07-18 20:18:27 -0500

    fixed link issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Antonio Gonzalez',
     description:
       '.. is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://google.com', // No trailing slash allowed!
+    siteUrl: 'https://hireantonio.com', // No trailing slash allowed!
     image: '', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },
